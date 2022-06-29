efec8e8c10c75077b5e4e27f55fde2240e56a354
Victor Nunez <VN135766@gmail.com>
2022-06-29 13:45:43 -0400

    defined collumns

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // define columns no need for id column sinc it will auto generate 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
